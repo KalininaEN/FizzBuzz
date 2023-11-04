e45ca4a9d0ed9fe345607f96558eb4ed8dc23082
Homework1 <kalinina_en@hotmail.com>
2023-11-04 21:28:58 +0300

    add for() rename variable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int numForFizz = 9;
-        if (numForFizz % 3 == 0 && numForFizz % 5 != 0) {
-            System.out.println("Fizz");
-        } else if (numForFizz % 5 == 0 && numForFizz % 3 != 0) {
-            System.out.println("Buzz");
-        } else if (numForFizz % 3 == 0 && numForFizz % 5 == 0) {
-            System.out.println("FizzBuzz");
+
+        for (int i = 0; i < 100; i++) {
+            if (i % 3 == 0 && i % 5 != 0) {
+                System.out.println("Fizz");
+
+            } else if (i % 5 == 0 && i % 3 != 0) {
+                System.out.println("Buzz");
+
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            }
         }
     }
 }
